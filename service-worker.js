74133dfd423cdf16b40cd01e030891a14289caaa
4cGTDXFxAYK6 <species05618@gmail.com>
2026-04-27 08:36:29 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ===============================
 //  PWA キャッシュ設定
 // ===============================
-const CACHE_VERSION = 'v76';   // ← 更新時は数字を上げる
+const CACHE_VERSION = 'v77';   // ← 更新時は数字を上げる
 const CACHE_NAME = `calculator-cache-${CACHE_VERSION}`;
 
 const URLS_TO_CACHE = [
